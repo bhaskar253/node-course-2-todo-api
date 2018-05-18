9f93c1fc1f518c98ff6507fcc3b554176227f24a
bhaskar253 <bhaskar.25392@gmail.com>
2018-05-18 23:20:26 +0530

    Added tests for DELETE /todos/:id

--- server/server.js
+++ server/server.js
@@ -38,7 +38,7 @@ app.get('/todos/:id',(req,res)=>{
   Todo.findById(id).then(todo=>{
     if(!todo)
       return res.status(404).send();
-    return res.send({todo});
+    res.send({todo});
   }).catch(e=>res.status(400).send());
 });
 
@@ -51,7 +51,7 @@ app.delete('/todos/:id',(req,res)=>{
   Todo.findByIdAndRemove(id).then(todo=>{
     if(!todo)
       return res.status(404).send();
-    res.send(todo);
+    res.send({todo});
   }).catch(e=>res.status(400).send());
 });
 
